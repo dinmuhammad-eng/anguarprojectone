ad3b3893138f34c3d00acb83beefe2119f4840d0
Din <dinmuhammadumar1@gmail.com>
2023-05-04 15:56:53 +0500

    there we added forms with validation and keep that to be shown in table using bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  dataArray: any[] = []; 
   title = 'xone';
+  name = '';
+  fname = '';
+  nname = '';
+  ename = '';
+
+  @ViewChild('logInForm') logInForm!: NgForm;
+
+  logInUser(data:any){
+    this.dataArray.push(data);
+    this.logInForm.reset();
+  }
 }
